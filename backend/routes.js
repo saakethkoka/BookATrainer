a4b1051baa6d7e9d7ba1cb8c02d890736eab3907
saakethkoka <saaketh.koka@gmail.com>
2021-10-28 18:31:49 -0500

    Created POST and GET for creating and retrieving certifications from trainers

--- backend/routes.js
+++ backend/routes.js
@@ -95,4 +95,73 @@ module.exports = function routes(app, logger) {
       }
     });
   });
+
+  // POST /createTrainerCertification
+  app.post('/createTrainerCertification', (req, res) => {
+    var trainer_id = req.body.trainer_id;
+    var amenity_name = req.body.amenity_name;
+    amenity_name = amenity_name.toLowerCase() // Making it so all activities are consistent
+    let activity_id
+    // obtain a connection from our pool of connections
+    pool.getConnection(function (err, connection){
+      if(err){
+        // if there is an issue obtaining a connection, release the connection instance and log the error
+        logger.error('Problem obtaining MySQL connection',err)
+        res.status(400).send('Problem obtaining MySQL connection');
+      } else {
+        // if there is no issue obtaining a connection, execute query and release connection
+        connection.query("INSERT INTO amenity_certifications (amenity_name) SELECT * FROM (SELECT ?) AS tmp WHERE NOT EXISTS (SELECT amenity_name FROM amenity_certifications WHERE amenity_name=?)", [amenity_name, amenity_name], function (err, rows, fields) {
+          if (err) {
+            // if there is an error with the query, log the error
+            logger.error("Problem inserting into test table: \n", err);
+            res.status(400).send('Problem inserting into activities table');
+          } else {
+            activity_id = rows.insertId
+          }
+        });
+
+        connection.query("INSERT INTO trainer_amenity_certifications(trainer_id, certification_id) VALUES (?, (SELECT certification_id FROM amenity_certifications WHERE amenity_name = ?))", [trainer_id, amenity_name], function (err, rows, fields) {
+          connection.release();
+          if (err) {
+            // if there is an error with the query, log the error
+            logger.error("Problem inserting into test table: \n", err);
+            res.status(400).send('Problem inserting into activities table');
+          } else {
+            res.status(400).send("Inserted activity into table and assigned it to the trainee successfully");
+          }
+        });
+
+      }
+    });
+  });
+
+  // GET /getTrainerCertifications/{trainer_id}
+  app.get('/getTrainerCertifications', (req, res) => {
+    var trainer_id = req.param('trainer_id');
+    // obtain a connection from our pool of connections
+    pool.getConnection(function (err, connection){
+      if(err){
+        // if there is an issue obtaining a connection, release the connection instance and log the error
+        logger.error('Problem obtaining MySQL connection',err)
+        res.status(400).send('Problem obtaining MySQL connection');
+      } else {
+        // if there is no issue obtaining a connection, execute query and release connection
+        connection.query("SELECT amenity_name FROM trainer_amenity_certifications tac INNER JOIN amenity_certifications ac on tac.certification_id = ac.certification_id WHERE trainer_id = ?;", [trainer_id], function (err, rows, fields) {
+          connection.release();
+          if (err) {
+            logger.error("Error while fetching values: \n", err);
+            res.status(400).json({
+              "data": [],
+              "error": "Error obtaining values"
+            })
+          } else {
+            res.status(200).json({
+              "data": rows
+            });
+          }
+        });
+      }
+    });
+  });
+
 }
