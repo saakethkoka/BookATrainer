325229e59492a8797d9d249b277041691efb9086
saakethkoka <63078934+saakethkoka@users.noreply.github.com>
2021-11-20 18:37:48 -0600

    created route to get a trainee's past trainers

--- backend/routes.js
+++ backend/routes.js
@@ -399,5 +399,34 @@ module.exports = function routes(app, logger) {
 
   })
 
+  // GET /pastTrainers/{trainee_id}
+  app.get('/pastTrainers', (req, res) => {
+    var trainee_id = req.param('trainee_id');
+    // obtain a connection from our pool of connections
+    pool.getConnection(function (err, connection){
+      if(err){
+        // if there is an issue obtaining a connection, release the connection instance and log the error
+        logger.error('Problem obtaining MySQL connection',err)
+        res.status(400).send('Problem obtaining MySQL connection');
+      } else {
+        // if there is no issue obtaining a connection, execute query and release connection
+        connection.query("SELECT * FROM past_trainers WHERE trainee_id = ?", trainee_id, function (err, rows, fields) {
+          connection.release();
+          if (err) {
+            logger.error("Error while fetching values: \n", err);
+            res.status(400).json({
+              "data": [],
+              "error": "Error obtaining values"
+            })
+          } else {
+            res.status(200).json({
+              "data": rows
+            });
+          }
+        });
+      }
+    });
+  });
+
 }
 
