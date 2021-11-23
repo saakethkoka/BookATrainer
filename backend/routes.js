c6c409525fdf69c201e9a4612f5613a9c75bb61c
saakethkoka <63078934+saakethkoka@users.noreply.github.com>
2021-11-22 23:44:34 -0600

    Created Route to get trainer_schedule

--- backend/routes.js
+++ backend/routes.js
@@ -409,6 +409,7 @@ module.exports = function routes(app, logger) {
       }
     });
   });
+
   // POST /createAppointment
   app.post('/createAppointment', (req, res) => {
     var trainer_id = req.body.trainer_id;
@@ -472,5 +473,32 @@ module.exports = function routes(app, logger) {
     });
   })
 
+  // GET /trainerSchedule/{trainer_id}
+  app.get('/trainerSchedule', (req, res) => {
+    var trainer_id = req.param("trainer_id");
+
+    // obtain a connection from our pool of connections
+    pool.getConnection(function (err, connection){
+      if(err){
+        // if there is an issue obtaining a connection, release the connection instance and log the error
+        logger.error('Problem obtaining MySQL connection',err)
+        res.status(400).send('Problem obtaining MySQL connection');
+      } else {
+        // if there is no issue obtaining a connection, execute query and release connection
+        connection.query('SELECT schedule_id, start_time, end_time FROM trainer_schedule WHERE trainer_id = ?', trainer_id, function (err, rows, fields) {
+          connection.release();
+          if (err) {
+            logger.error("Error while fetching values: \n", err);
+            res.status(400).json({
+              "data": [],
+              "error": "Error obtaining values"
+            })
+          } else {
+            res.status(200).json(rows);
+          }
+        });
+      }
+    });
+  });
 
 }
