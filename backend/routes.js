289ad9ebe47932da113be8e83c881ceed1073051
saakethkoka <63078934+saakethkoka@users.noreply.github.com>
2021-11-30 17:41:08 -0600

    Created way to retrieve chat messages.

--- backend/routes.js
+++ backend/routes.js
@@ -1151,4 +1151,36 @@ pool.getConnection(function (err, connection){
 
     });
 
+    // GET /getChatMessages/{user_1}/{user_2} -Saaketh
+    app.get('/getChatMessages', (req, res) => {
+        var user_1 = req.param("user_1");
+        var user_2 = req.param("user_2");
+        // obtain a connection from our pool of connections
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                // if there is an issue obtaining a connection, release the connection instance and log the error
+                logger.error('Problem obtaining MySQL connection', err)
+                res.status(400).send('Problem obtaining MySQL connection');
+            } else {
+                // if there is no issue obtaining a connection, execute query and release connection
+                connection.query('SELECT * FROM chat_messages WHERE (from_user = ? AND to_user = ?) OR (from_user = ? AND to_user = ?) ORDER BY time', [user_1, user_2, user_2, user_1], function (err, rows, fields) {
+                    connection.release();
+                    if (err) {
+                        logger.error("Error while fetching values: \n", err);
+                        res.status(400).json({
+                            "data": [],
+                            "error": "Error obtaining values"
+                        })
+                    } else {
+                        res.status(200).json({
+                            "data": rows
+                        });
+                    }
+                });
+            }
+        });
+    });
+
+
+
 }
