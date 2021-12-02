1919f44466f7ee2b5f917de973effff7dfa1e059
saakethkoka <63078934+saakethkoka@users.noreply.github.com>
2021-12-01 19:44:55 -0600

    Created route to sort past trainers based on how manu times a trainee has trained with them

--- backend/routes.js
+++ backend/routes.js
@@ -320,7 +320,6 @@ module.exports = function routes(app, logger) {
             }
         });
     });
-
     // get a list of all activities offered by the whole site
     app.get('/activities', (req, res) => {
     // obtain a connection from our pool of connections
@@ -1304,6 +1303,36 @@ pool.getConnection(function (err, connection){
                     connection.release();
                     if (err) {
                         logger.error("Error while fetching values: \n", err);
+                        res.status(400).json({
+                            "data": [],
+                            "error": "Error obtaining values"
+                        })
+                    } else {
+                        res.status(200).json({
+                            "data": rows
+                        });
+                    }
+                });
+            }
+        });
+    });
+
+    // GET /sortedPastTrainers/{trainee_id}
+    app.get('/sortedPastTrainers', (req, res) => {
+        var trainee_id = req.query.trainee_id
+        // obtain a connection from our pool of connections
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                // if there is an issue obtaining a connection, release the connection instance and log the error
+                logger.error('Problem obtaining MySQL connection', err)
+                res.status(400).send('Problem obtaining MySQL connection');
+            } else {
+                // if there is no issue obtaining a connection, execute query and release connection
+                connection.query("SELECT trainer_id, COUNT(*) as count FROM appointments WHERE trainee_id = ? GROUP BY trainer_id ORDER BY COUNT(*) DESC;", [trainee_id], function (err, rows, fields) {
+                    connection.release();
+                    if (err) {
+                        // if there is an error with the query, log the error
+                        logger.error("Problem getting favorite trainers: \n", err);
                         res.status(400).json({
                             "data": [],
                             "error": "Error obtaining values"
