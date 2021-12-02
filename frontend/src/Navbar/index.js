77f19468b06664674251fb4e22035654cca0011e
Eileen Garcia <eileeng@smu.edu>
2021-12-02 14:54:53 -0600

    NavBar Contact Us MailTo Up and Running

--- frontend/src/Navbar/index.js
+++ frontend/src/Navbar/index.js
@@ -11,6 +11,11 @@ import React from 'react';
 
 const Navbar = () => {
   let helpEmail = "bookATrainerHelp@gmail.com";
+  let mailTo = "mailto:" + helpEmail;
+
+  const onMail = (event) => {
+    window.location = mailTo;
+  }
 
   return (
     <>
@@ -23,7 +28,7 @@ const Navbar = () => {
           <NavLink to='/trainers' activeStyle>
             Trainers
           </NavLink>
-          <NavLink to={{pathname: "mailto:" + helpEmail}} activeStyle>
+          <NavLink to='/contact-us' onClick={ event => onMail(mailTo) } activeStyle>
             Contact Us
           </NavLink>
           <NavLink to='/sign-up' activeStyle>
